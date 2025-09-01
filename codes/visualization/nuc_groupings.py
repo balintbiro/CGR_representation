4f90c7157c2ff940867673620fc010a5eb043c60
Bálint Biró <76958102+balintbiro@users.noreply.github.com>
2025-09-01 13:39:28 +0200

    Add sample sequence representation

--- codes/visualization/nuc_groupings.py
+++ codes/visualization/nuc_groupings.py
@@ -1,9 +1,4 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-
-plt.figure(figsize=(3,3))
-# Create the graph
+fig,axs=plt.subplots(1,2)
 G = nx.Graph()
 G.add_edge("A", "C")
 G.add_edge("A", "G")
@@ -13,19 +8,20 @@ G.add_edge("A", "T")
 pos = nx.spring_layout(G)  # You can also define custom positions
 
 # Draw nodes and labels
-nx.draw_networkx_nodes(G, pos, node_color="#000000")
-nx.draw_networkx_labels(G, pos, font_color="#ffffff")
+nx.draw_networkx_nodes(G, pos, node_color="#000000",ax=axs[0])
+nx.draw_networkx_labels(G, pos, font_color="#ffffff",ax=axs[0])
 
 kwargs={"linestyle":"--"}
 
-connection_width=8
+connection_width=6
 # Draw curved edge A-G using connectionstyle
 nx.draw_networkx_edges(
     G, pos,
     edgelist=[("A", "C")],
     edge_color="#bd8e83",
     arrows=True,
-    width=connection_width
+    width=connection_width,
+    ax=axs[0]
 )
 nx.draw_networkx_edges(
     G, pos,
@@ -33,14 +29,16 @@ nx.draw_networkx_edges(
     edge_color="#bd8e83",
     arrows=True,
     style="--",
-    width=connection_width
+    width=connection_width,
+    ax=axs[0]
 )
 nx.draw_networkx_edges(
     G, pos,
     edgelist=[("A", "G")],
     edge_color="#e9dac4",
     arrows=True,
-    width=connection_width
+    width=connection_width,
+    ax=axs[0]
     #connectionstyle="arc3,rad=.3"  # Adjust rad for curvature
 )
 nx.draw_networkx_edges(
@@ -50,14 +48,16 @@ nx.draw_networkx_edges(
     arrows=True,
     style="--",
     width=connection_width,
-    label="Strong"
+    label="Strong",
+    ax=axs[0]
 )
 nx.draw_networkx_edges(
     G, pos,
     edgelist=[("A", "T")],
     edge_color="#749eb2",
     arrows=True,
-    width=connection_width
+    width=connection_width,
+    ax=axs[0]
     #connectionstyle="arc3,rad=.3"  # Adjust rad for curvature
 )
 nx.draw_networkx_edges(
@@ -66,7 +66,8 @@ nx.draw_networkx_edges(
     edge_color="#749eb2",
     arrows=True,
     style="--",
-    width=connection_width
+    width=connection_width,
+    ax=axs[0]
 )
 
 
@@ -75,10 +76,42 @@ legend_elements = [
     Line2D([0], [0], color='#e9dac4', lw=0, marker='o', label='Structure'),
     Line2D([0], [0], color='#749eb2', lw=0, marker='o', label='H-bond')
 ]
+axs[0].legend(handles=legend_elements,bbox_to_anchor=(0.5,-.1))
 
-plt.legend(handles=legend_elements, loc="best")#bbox_to_anchor=(1,0.75)
+for spine in axs[0].spines.values():
+    spine.set_visible(False)
 
 
-plt.axis("off")
+coords,firms,dash=CGRepresentation(seq="ACGT",encodings=side_groups)
+for index,firm in enumerate(firms):
+    axs[1].plot(coords[index+1][0],coords[index+1][1],marker="o",color="k")
+    axs[1].plot(firm[0],firm[1],linestyle="-",color="k")
+    axs[1].plot(dash[index][0],dash[index][1],linestyle="--",color="k",alpha=.5)
+
+fontsize=20
+
+axs[1].text(x=-1.25,y=1,s='A',fontsize=fontsize,color="#749eb2",weight="semibold")
+axs[1].text(x=-1.25,y=1.25,s='A',fontsize=fontsize,color="#e9dac4",weight="semibold")
+axs[1].text(x=-1.25,y=1.5,s='A',fontsize=fontsize,color="#bd8e83",weight="semibold")
+
+axs[1].text(x=-1.4,y=-1.25,s='AC',fontsize=fontsize,color="#bd8e83",weight="semibold")
+axs[1].text(x=-1.4,y=-1.5,s='AG',fontsize=fontsize,color="#e9dac4",weight="semibold")
+axs[1].text(x=-1.4,y=-1.75,s='AT',fontsize=fontsize,color="#749eb2",weight="semibold")
+
+axs[1].text(x=1,y=-1.25,s='ACG',fontsize=fontsize,color="#bd8e83",weight="semibold")
+axs[1].text(x=1,y=-1.5,s='AGC',fontsize=fontsize,color="#e9dac4",weight="semibold")
+axs[1].text(x=1,y=-1.75,s='ATC',fontsize=fontsize,color="#749eb2",weight="semibold")
+
+axs[1].text(x=1,y=1,s='ATCG',fontsize=fontsize,color="#749eb2",weight="semibold")
+axs[1].text(x=1,y=1.25,s='AGCT',fontsize=fontsize,color="#e9dac4",weight="semibold")
+axs[1].text(x=1,y=1.5,s='ACGT',fontsize=fontsize,color="#bd8e83",weight="semibold")
+
+axs[1].set(
+    xlabel="",ylabel="",
+    xticks=[],yticks=[]
+)
+
+
+axs[1].set(xlim=(-1,1),ylim=(-1,1))
 plt.tight_layout()
-plt.savefig("nuc_groupings.png")
+plt.savefig("combined_groupings.png",dpi=300)
