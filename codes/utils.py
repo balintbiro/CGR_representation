9199df5677289c47d9f549acfe5d2aaf7be0cfba
Bálint Biró <76958102+balintbiro@users.noreply.github.com>
2025-09-09 11:29:37 +0200

    Correct FCGR
    Orientation adjusted FCGR for visualization and further analysis.

--- codes/utils.py
+++ codes/utils.py
@@ -17,51 +17,92 @@ bonds={
     'T':[-1,-1]
 }
 
-def CGRepresentation(seq:str,encodings:dict)->list:
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+
+side_groups={
+    'A':[-1,1],
+    'C':[-1,-1],
+    'G':[1,-1],
+    'T':[1,1]
+}
+structure={
+    'A':[-1,1],
+    'C':[1,-1],
+    'G':[-1,-1],
+    'T':[1,1]
+}
+bonds={
+    'A':[-1,1],
+    'C':[1,1],
+    'G':[1,-1],
+    'T':[-1,-1]
+}
+
+def CGRepresentation(
+    sequence:str,
+    encodings:dict,
+    scaling_factor:float
+)->pd.DataFrame:
     """
     Generating Chaos Game Representations (point coordinates) for DNA sequences.
 
     Parameters:
-    - seq: DNA sequence with datatype of str
+    - sequence: DNA sequence with datatype of str
     - encodings: dictionary type variable that contains values for all of the nucleotides
+    - scaling_factor: float for scaling the steps between nucleotides
 
     Returns:
-    - list of coordinates
+    - dataframe of coordinates
     """
-    seq=seq.replace("\n",'').replace('N','')
+    sequence=sequence.replace("\n",'').replace('N','')
     coordinates=[[0,0]]
-    for nuc in seq:
-        corner=encodings[nuc]
-        current=coordinates[-1]
-        x=(corner[0]+current[0])/2
-        y=(corner[1]+current[1])/2
-        coordinates.append([x,y])
-    return coordinates[1:]
 
-def FrequencyCGR(coordinates, resolution):
+    for nucleotide in sequence:
+        corner=encodings[nucleotide]
+        current=coordinates[-1]
+        x=scaling_factor*(corner[0]+current[0])
+        y=scaling_factor*(corner[1]+current[1])
+        coordinates.append([x,y])
+        
+    return pd.DataFrame(
+        data=coordinates[1:],
+        columns=list("xy")
+    )
+
+def FrequencyCGR(
+        coordinates:pd.DataFrame,
+        resolution:float,
+        flatten=True
+)->np.array:
     """
     Generate a Frequency Chaos Game Representation (FCGR) matrix.
 
     Parameters:
-    - coordinates: list of tuples (x, y) representing points in the unit square [0, 1] x [0, 1]
-    - resolution: int, the number of bins along each axis (e.g., 256 for a 256x256 matrix)
+    - coordinates: dataframe of x,y coordinates representing points in the unit square [-1, 1] x [-1, 1]
+    - resolution: int, the number of bins along each axis (e.g., 8 for a 8x8 matrix)
+    - flatten: bool, for visualization purposes, flatten=False should be used
 
     Returns:
-    - fcgr_matrix: 2D numpy array of shape (resolution, resolution)
+    - fcgr_matrix: 1D (2D if flatten=False is applied) numpy array of shape (resolution, resolution)
     """
+    bins=np.linspace(start=-1.01,stop=1.01,num=resolution+1)
+    labels=np.linspace(start=0,stop=resolution-1,num=resolution,dtype=int)
+    
+    categories=pd.DataFrame(columns=[list("xy")])
+    categories['x']=pd.cut(x=coordinates.x,bins=bins,labels=labels)
+    categories['y']=pd.cut(x=coordinates.y,bins=bins,labels=labels)
+
     fcgr_matrix = np.zeros((resolution, resolution), dtype=int)
 
-    for x, y in coordinates:
-        i = int(x * resolution)
-        j = int(y * resolution)
-
-        # Ensure indices are within bounds
-        i = min(i, resolution - 1)
-        j = min(j, resolution - 1)
-
-        fcgr_matrix[i, j] += 1
-
-    return fcgr_matrix.flatten()
+    for index,row in categories.iterrows():
+        fcgr_matrix[row.x,row.y] += 1
+    fcgr_matrix=np.rot90(m=fcgr_matrix,axes=(-2,-1))
+    if flatten:
+        return fcgr_matrix.flatten()
+    else:
+        return fcgr_matrix
 
 def tester(mtx:pd.DataFrame,dataset_name:str,skf:StratifiedKFold)->pd.DataFrame:
     """
