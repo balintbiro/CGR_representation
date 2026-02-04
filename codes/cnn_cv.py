f7a446e031247101cf8267e7fc249b7566197dc8
balintbiro <biro.balint@uni-mate.hu>
2026-02-04 09:43:32 +0100

    Tested, working cnn cv

--- codes/cnn_cv.py
+++ codes/cnn_cv.py
@@ -18,7 +18,7 @@ device="cuda" if torch.cuda.is_available() else "cpu"
 
 logger=logging.getLogger(__name__)
 
-def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.array,n_split=3)->list:
+def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.ndarray,n_split=3)->list:
     skf=StratifiedKFold(n_splits=n_split,random_state=0,shuffle=True)
     acc_scores=[]
     for i, (train_index, test_index) in enumerate(skf.split(X, y)):
@@ -38,3 +38,67 @@ def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.array,n_split=3)->li
         acc_score=accuracy_score(y_true=y_test,y_pred=y_pred)
         acc_scores.append(acc_score)
     return acc_scores
+
+@click.command()
+@click.option(
+    "--logfile",
+    help="Path to logfile[.log]",
+    required=True
+)
+@click.option(
+    "--fcgr_matrix",
+    help="Path to file[.csv] containing FCGRs",
+    required=True
+)
+@click.option(
+    "--outfile",
+    help="Path to outfile[.csv] that will contain the distances",
+    required=True
+)
+@click.option(
+    "--res",
+    help="Resolution (int) of the FCGR",
+    required=True,
+    type=int
+)
+@click.option(
+    "--n",
+    help="Number of cross validation splits",
+    required=True,
+    type=int
+)
+
+def main(
+        logfile,
+        fcgr_matrix,
+        outfile,
+        res,
+        n
+    )->None:
+    """
+    Perform CNN based cross validation on FCGR matrix.
+
+    Parameters:
+    - logfile: Path to logfile[.log]
+    - fcgr_matrix: Path to file[.csv] containing FCGRs
+    - outfile: Path to outfile[.csv] that will contain the accuracies
+    - res: Resolution (int) of the FCGR
+    - n: Number of cross validation splits
+
+    Returns:
+    - None
+    """
+    loggerConfig(logfile=logfile)
+    logger.info("Loading FCGR matrix from %s",fcgr_matrix)
+    fcgr_df=pd.read_csv(fcgr_matrix)
+    X,y=fcgr_df.drop(columns=["label"]).values.astype("float32"),fcgr_df["label"].values.astype("float32")
+    XCnn=X.reshape(-1,1,res,res)
+    logger.info("Starting cross validation with %d splits",n)
+    acc_scores=cross_validate(X=XCnn,y=y,n_split=n)
+    acc_df=pd.DataFrame()
+    acc_df["accuracy"]=acc_scores
+    acc_df.to_csv(outfile,index=False)
+    logger.info("Accuracies saved to %s",outfile)
+
+if __name__ == "__main__":
+    main()
