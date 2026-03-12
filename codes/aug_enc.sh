a65ec78ecb8f28300e3b8f2b20135ff6039f06b0
balintbiro <biro.balint@uni-mate.hu>
2026-03-12 09:19:40 +0100

    Standalone dir for augmnetation datasets.
    Min,q1,q2,q3 and max datasets are written into a separate directory. This way the actual augmentation would be easier to perform.

--- codes/aug_enc.sh
+++ codes/aug_enc.sh
@@ -3,16 +3,22 @@
 #these are the encodings that have the 2500th, 5000th and 7000th
 #indices in the sorted list of 10000 random encodings
 encodings=(
+    QYIPKRATWDVHSMFCEGNL
     PQGLATRKEDCNIHFWYVMS
     EAKYNFMRHQPIWLDSVCGT
     TSQNGYIHWMAKFLVDEPRC
+    RGSYEDQTKHWMVLFNPIAC
 )
 
 out_filenames=(
-    2500
-    5000
-    7500
+    min
+    q1
+    q2
+    q3
+    max
 )
+
+mkdir -p ../data/spec_datasets
 
 #running FCGR generation for each of the three encodings and saving the output in a csv file
 for i in "${!encodings[@]}"
@@ -21,6 +27,6 @@ do
     --encoding "${encodings[$i]}" \
     --scaling_factor 0.865 \
     --resolution 35 \
-    --output_file "data/${out_filenames[$i]}.csv"\
-    --input_filename data/deeploc_clean.csv
+    --output_file "../data/spec_datasets/${out_filenames[$i]}.csv"\
+    --input_filename "../data/deeploc_clean.csv"
 done
