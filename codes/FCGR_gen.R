06b87c4734d091471728ae480c254d960fb3dae4
balintbiro <biro.balint@uni-mate.hu>
2026-03-23 10:32:07 +0100

    Delete boilerplate.
    R code was containing too much information since it specified use cases for DNA, numbers etc. By deleting these parts, hopefully the run will be faster.

--- codes/FCGR_gen.R
+++ codes/FCGR_gen.R
@@ -59,10 +59,6 @@ distr.pts = function(n,
     x[i] = r*sinpi((2*i+1)/n)
     y[i] = r*cospi((2*i+1)/n)
   }
-
-  #generates a plot if required
-  if (plot) {plot(x, y, pch = 20)}
-
   #return coordinates
   return(xy.coords(x, y))
 }
@@ -72,73 +68,21 @@ cgr = function(data,
                index,
                label,
                seq.base = row.names(table(data)),
-               sf = F,
-               res = 100) {
+               sf,
+               res) {
 
   r = 1
   if(is.character(seq.base)&&length(seq.base)==1){
-    if(seq.base == "digits"){
-      seq.base =c(0:9)
-    }
-    else if(seq.base == "AMINO"){
-      #seq.base=c("A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R",
-       #          "S","T","V","W","Y")
+    if(seq.base == "AMINO"){
        seq.base=strsplit(encoding,"")[[1]]
     }
-    else if(seq.base == "amino"){
-      seq.base=c("a","c","d","e","f","g","h","i","k","l","m","n","p","q","r",
-                 "s","t","v","w","y")
-    }
-
-    else if (seq.base == "DNA"){
-      seq.base= c("A","G","T","C")
-    }
-
-    else if (seq.base == "dna"){
-      seq.base= c("a","g","t","c")
-    }
-    else if (seq.base == "LETTERS"){
-      seq.base=LETTERS
-    }
-
-    else if (seq.base == "letters"){
-      seq.base=letters
-    }
-
   }
   data=strsplit(data,"")[[1]]
 
-  #check for input errors
-  stopifnot(
-    length(seq.base) >= length(table(data)),
-    all(row.names(table(data)) %in% seq.base),
-    sf <= 1,
-    sf >= 0,
-    res >= 1)
-
-
-
-
   #get the number of bases
   base.num = length(seq.base)
-
-  if(base.num==4){
-    x=c(1,-1,-1,1)
-    y=c(-1,-1,1,1)
-    base.coord = xy.coords(x, y)
-  }
-  #calculate corner coordinates for the base
-  else{
-
-    base.coord = distr.pts(base.num, r)
-  }
-
-  #calculate the "scaling factor" depending on the number of bases.
-  #the scaling factor is the ratio of the radius of the circle that
-  #passes through the centers of the touching subpolygons to the
-  #radius of a circle that circumscribes the parent polygon.
-  if (!sf) {sf =  1- (sinpi(1/base.num)/ (sinpi(1 / base.num) + sinpi (
-    1/base.num + 2 * (floor (base.num/4) /base.num))))}
+  #calculate coordinates for the base
+  base.coord = distr.pts(base.num, r)
 
   #data frame for easy access
   base = data.frame(x = base.coord$x,
