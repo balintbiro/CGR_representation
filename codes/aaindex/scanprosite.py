45755013b4c618cf26128b9be46009c5bbfb98e3
balintbiro <biro.balint@uni-mate.hu>
2026-07-20 14:09:35 +0200

    Accepted conflicting changes

--- codes/aaindex/scanprosite.py
+++ codes/aaindex/scanprosite.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import subprocess
 
-from utils import loggerConfig,ProSite
+from codes.utils import loggerConfig,ProSite
 
 logger=logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -35,11 +35,7 @@ def main(logfile:str,seqfile:str,outdir:str)->None:
     if os.path.exists(os.path.join(outdir,"prosite_motives.csv")):
         pass
     else:
-<<<<<<< HEAD
         out_df=pd.DataFrame(columns=["sequence_id","signature_ac","start","stop"])
-=======
-        out_df=pd.DataFrame(columns=["sequence_ac","start","stop","signature_ac","score","level","sequence",])
->>>>>>> 5cb608f (Scanprosite code and log)
         out_df.to_csv(os.path.join(outdir,"prosite_motives.csv"),index=False)
     if os.path.exists(os.path.join(outdir,"prosite_signatures.csv")):
         pass
@@ -50,15 +46,10 @@ def main(logfile:str,seqfile:str,outdir:str)->None:
     script_name=os.path.basename(__file__)
     logger.info(f"Filename: {script_name} started.")
     sequences=pd.read_csv(seqfile)
-<<<<<<< HEAD
     accessions=[]
     for index,sequence in enumerate(sequences["sequence"]):
         seqid=sequences["id"].values[index]
         prosite=ProSite(sequence=sequence,id=seqid)
-=======
-    for index,sequence in enumerate(sequences["sequence"][:5]):
-        prosite=ProSite(sequence=sequence)
->>>>>>> 5cb608f (Scanprosite code and log)
         try:
             # find motives in a particular sequence
             motives=prosite.find_motives()
