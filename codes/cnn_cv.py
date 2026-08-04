5b49a5769d99eed0fe5451b6d2b429af85e3f9b9
balintbiro <biro.balint@uni-mate.hu>
2026-08-04 14:51:54 +0200

    Full CV code.
    Needs attention as AUROC with ResNet is nonsense.

--- codes/cnn_cv.py
+++ codes/cnn_cv.py
@@ -1,5 +1,6 @@
 # import the necessary libraries
 import os
+import sys
 import click
 import torch
 import random
@@ -9,24 +10,33 @@ import numpy as np
 import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
-from skorch import NeuralNetBinaryClassifier
+from skorch import NeuralNetBinaryClassifier,NeuralNetClassifier
+from pathlib import Path
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
 
-from codes.utils import loggerConfig,Cnn
+from codes.utils import loggerConfig,Cnn,ResNet
+
+HERE=Path(__file__).resolve().parent
+PROJECT_ROOT=HERE.parent.parent
+CODES=PROJECT_ROOT / "codes"
+DATA=PROJECT_ROOT / "data"
+RESULTS=PROJECT_ROOT / "results"
 
 device="cuda" if torch.cuda.is_available() else "cpu"
 
 logger=logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
-def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.ndarray,n_split=3)->list:
+def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.ndarray,task:str,model:str,n_split=3)->list:
     """
     Perform cross validation on the given data.
 
     Parameters:
     - X: DataFrame or numpy array containing the features.
     - y: Series or numpy array containing the labels.
+    - task: binary|multiclass
     - n_split: Number of splits for cross validation. 10 was used for the publication.
 
     Returns:
@@ -38,25 +48,71 @@ def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.ndarray,n_split=3)->
     np.random.seed(seed)
     random.seed(seed)
     skf=StratifiedKFold(n_splits=n_split,random_state=seed,shuffle=True)
-    acc_scores=[]
+    scores=[]
+    if task=="binary":
+        y=y.values.astype("float32")
+    elif task=="multiclass":
+        y=y.values.astype(np.int64)
     for i, (train_index, test_index) in enumerate(skf.split(X, y)):
         X_train, X_test, y_train, y_test=X[train_index],X[test_index],y[train_index],y[test_index]
-
-        cnn = NeuralNetBinaryClassifier(
-            Cnn,
-            max_epochs=10,
-            lr=0.001,
-            optimizer=torch.optim.Adam,
-            device=device,
-        )
-
+        seed=0
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
+        if task=="binary":
+            if model=="custom":
+                cnn=NeuralNetBinaryClassifier(
+                    Cnn(output_dim=1),
+                    max_epochs=10,
+                    lr=0.001,
+                    optimizer=torch.optim.Adam,
+                    device=device,
+                    train_split=None,
+                    iterator_train__shuffle=None
+                )
+            elif model=="resnet":
+                cnn=NeuralNetBinaryClassifier(
+                    ResNet(output_dim=1),
+                    max_epochs=10,
+                    lr=0.001,
+                    optimizer=torch.optim.Adam,
+                    device=device,
+                    train_split=None,
+                    iterator_train__shuffle=None
+                )
+        elif task=="multiclass":
+            if model=="custom":
+                cnn=NeuralNetClassifier(
+                    Cnn(output_dim=5),
+                    criterion=torch.nn.CrossEntropyLoss,
+                    max_epochs=10,
+                    lr=0.001,
+                    optimizer=torch.optim.Adam,
+                    device=device,
+                    train_split=None,
+                    iterator_train__shuffle=None
+                )
+            elif model=="resnet":
+                cnn=NeuralNetClassifier(
+                    ResNet(output_dim=5),
+                    criterion=torch.nn.CrossEntropyLoss,
+                    max_epochs=10,
+                    lr=0.001,
+                    optimizer=torch.optim.Adam,
+                    device=device,
+                    train_split=None,
+                    iterator_train__shuffle=None
+                )
         cnn.fit(X_train,y_train)
-        y_pred=cnn.predict(X_test)
-        acc_score=accuracy_score(y_true=y_test,y_pred=y_pred)
-        acc_scores.append(acc_score)
+        if task=="binary":
+            auroc=roc_auc_score(y_true=y_test,y_score=cnn.predict_proba(X_test)[:,1])
+        elif task=="multiclass":
+            auroc=roc_auc_score(y_true=y_test,y_score=cnn.predict_proba(X_test),average="macro",multi_class="ovr")
+        scores.append([model,auroc])
         if (i>0) and (i%5==0):
             logger.info("Completed %d/%d folds",i,n_split)
-    return acc_scores
+            logger.info(f"Used model is: {cnn}")
+    return scores
 
 # define the command line interface using click
 @click.command()
@@ -81,10 +137,37 @@ def cross_validate(X:pd.DataFrame|np.ndarray,y:pd.Series|np.ndarray,n_split=3)->
     required=True
 )
 @click.option(
+    "--model",
+    help="Name of the model to use.",
+    required=True,
+    type=click.Choice(
+        ["custom","resnet"],
+        case_sensitive=False
+    )
+)
+@click.option(
+    "--task",
+    help="Name of the task to perform",
+    required=True,
+    type=click.Choice(
+        ["binary","multiclass"],
+        case_sensitive=False
+    )
+)
+@click.option(
     "--res",
     help="Resolution (int) of the FCGR",
     required=True,
     type=int
+)
+@click.option(
+    "--rank",
+    help="Rank of the dataset (min...max)",
+    required=True,
+    type=click.Choice(
+        ["min","q1","q2","q3","max"],
+        case_sensitive=False
+    )
 )
 @click.option(
     "--n",
@@ -98,7 +181,10 @@ def main(
         fcgr_matrix,
         outfile,
         name,
+        model,
+        task,
         res,
+        rank,
         n
     )->None:
     """
@@ -117,21 +203,21 @@ def main(
     if os.path.exists(outfile):
         pass
     else:
-        out_df=pd.DataFrame(columns=["Accuracy","Name"])
+        out_df=pd.DataFrame(columns=["model","auroc","dataset","rank"])
         out_df.to_csv(outfile,index=False)
     loggerConfig(logfile=logfile)
     logger.info("Loading FCGR matrix from %s",fcgr_matrix)
     fcgr_df=pd.read_csv(fcgr_matrix)
-    X,y=fcgr_df.drop(columns=["label"]).values.astype("float32"),fcgr_df["label"].values.astype("float32")
+    X,y=fcgr_df.drop(columns=["label"]).div(fcgr_df.drop(columns=["label"]).max(axis=1),axis=0).values.astype("float32"),fcgr_df["label"]
     XCnn=X.reshape(-1,1,res,res)
     logger.info("Starting cross validation with %d splits",n)
-    acc_scores=cross_validate(X=XCnn,y=y,n_split=n)
-    # save the accuracies in the output file
-    acc_df=pd.DataFrame()
-    acc_df["Accuracy"]=acc_scores
-    acc_df["Name"]=name
-    acc_df.to_csv(outfile,index=False,header=False,mode="a")
-    logger.info("Accuracies saved to %s",outfile)
+    scores=cross_validate(X=XCnn,y=y,task=task,model=model,n_split=n)
+    # save the scores in the output file
+    scores_df=pd.DataFrame(scores)
+    scores_df["dataset"]=name
+    scores_df["rank"]=rank
+    scores_df.to_csv(outfile,index=False,header=False,mode="a")
+    logger.info("Scores saved to %s",outfile)
 
 if __name__ == "__main__":
     main()
